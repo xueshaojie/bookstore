01cf932dbc55df3c114cc1a48746dae68f65a0cd
xueshaojie <xueshaojie8@gmail.com>
2017-06-03 13:52:35 +0800

    modify carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,19 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
+  if Rails.env.production?
+   config.fog_provider = 'fog'
    config.storage             = :qiniu
    config.qiniu_access_key    = ENV["HIS34ruiqlzti-BKZ5pJ3qiulFPc3DOqM8y3Kocc"]
    config.qiniu_secret_key    = ENV["FIOyGjMcvEm0T_rP8ErVgFfQdgR14Di0ztKHdF_H"]
    config.qiniu_bucket        = ENV["bookstore"]
    config.qiniu_bucket_domain = ENV["oqnq4jj8y.bkt.clouddn.com"]
    config.qiniu_block_size    = 4*1024*1024
-   #config.qiniu_protocol      = "http"
-   #config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，up.qiniug.com 不同
- end
+   config.qiniu_protocol      = "http"
+   config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，up.qiniug.com 不同
+  else
+    config.storage :file
+  end
+end
