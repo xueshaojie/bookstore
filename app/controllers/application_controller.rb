853883e4ed39f145576a443089dc91029f934a3f
xueshaojie <xueshaojie8@gmail.com>
2017-06-05 14:51:26 +0800

    modify seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,20 +39,6 @@ end
 
 module RandomCode
   class << self
-    def generate_password len = 8
-      seed = (0..9).to_a + ('a'..'z').to_a + ('A'..'Z').to_a + ['!', '@', '#', '$', '%', '.', '*'] * 4
-      token = ""
-      len.times { |t| token << seed.sample.to_s }
-      token
-    end
-
-    def generate_cellphone_token len = 6
-      a = lambda { (0..9).to_a.sample }
-      token = ""
-      len.times { |t| token << a.call.to_s }
-      token
-    end
-
     def generate_utoken len = 8
       a = lambda { rand(36).to_s(36) }
       token = ""
