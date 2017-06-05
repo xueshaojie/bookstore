ec3cef2a34580887776f738bdb90b0c209c7bd9c
xueshaojie <xueshaojie8@gmail.com>
2017-06-05 16:38:55 +0800

    modify qiniu access_key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
     config.storage             = :qiniu
-    config.qiniu_access_key    = ENV["NvUplOFouFIWjvqztMBoFlBVu0VkuWyArXfGG4TD"]
-    config.qiniu_secret_key    = ENV["g9QjdWFVKEB7dpp6iqn-0NMnqe6jyr-Ak1Ebjh48"]
+    config.qiniu_access_key    = ENV["qNWUPFkb-NqV_G76cd5tJIULSb5wRXz6DZql8Rzu"]
+    config.qiniu_secret_key    = ENV["fKc8laGx8xd6IEkzDDnbpNwPwSJsOwVKM2gPAUJ9"]
     config.qiniu_bucket        = ENV["bookstore"]
     config.qiniu_bucket_domain = ENV["oqnq4jj8y.bkt.clouddn.com"]
     config.qiniu_block_size    = 4*1024*1024
