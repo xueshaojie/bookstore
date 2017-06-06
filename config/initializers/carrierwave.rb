6e7e19ed9d5f013a76d9d5b65949691d2f2a2682
xueshaojie <xueshaojie8@gmail.com>
2017-06-06 16:08:21 +0800

    modify qiniu

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
-    config.storage             = :qiniu
-    config.qiniu_access_key    = ENV["qNWUPFkb-NqV_G76cd5tJIULSb5wRXz6DZql8Rzu"]
-    config.qiniu_secret_key    = ENV["fKc8laGx8xd6IEkzDDnbpNwPwSJsOwVKM2gPAUJ9"]
-    config.qiniu_bucket        = ENV["bookstore"]
-    config.qiniu_bucket_domain = ENV["oqnq4jj8y.bkt.clouddn.com"]
-    config.qiniu_block_size    = 4*1024*1024
-    config.qiniu_protocol      = "http"
-    config.qiniu_up_host       = "http://up.qiniug.com"
+  config.storage             = :qiniu
+  config.qiniu_access_key    = ENV["oLrzyKW99Ja4Eiguujl0u459Qt6RxNmaj0SukqA8"]
+  config.qiniu_secret_key    = ENV["y_dYwghS4AmmYbiBFdmfrZ4xjyqMepyk7GgdXbFl"]
+  config.qiniu_bucket        = ENV["bookstore1"]
+  config.qiniu_bucket_domain = ENV["or48vjut5.bkt.clouddn.com"]
+  config.qiniu_block_size    = 4*1024*1024
+  config.qiniu_protocol      = "http"
+  config.qiniu_up_host       = "http://up.qiniug.com"
 end
