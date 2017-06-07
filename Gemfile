da0560ee21801a7352b2ade3729e0abb3a0a8164
xueshaojie <xueshaojie8@gmail.com>
2017-06-07 15:00:08 +0800

    add figaro

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'will_paginate'
 gem 'rest-client'
 gem 'acts_as_votable', '~> 0.10.0'
 gem 'qiniu-rs'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["F38HFE5FogMvc0JvHMDltga5yNYUrQMimLVrvTT1"]
-  config.qiniu_secret_key    = ENV["WorF2Oz5LfPlmj5RgwFGEaDa8A6_d3JU7sJ96m8s"]
-  config.qiniu_bucket        = ENV["bookstore"]
-  config.qiniu_bucket_domain = ENV["oqnq4jj8y.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up.qiniug.com"
