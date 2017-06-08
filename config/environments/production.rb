8c5de110bf5d70bc71a2c8ed9cceb7bda869dc40
xueshaojie <xueshaojie8@gmail.com>
2017-06-08 09:09:49 +0800

    modify public

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'https://pacific-hamlet-62236.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'https://shrouded-spire-34080.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
   config.qiniu_bucket        = ENV["qiniu_bucket"]
   config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
-  config.qiniu_protocol      = "https"
-  config.qiniu_up_host       = "https://up.qiniug.com"
+  config.qiniu_protocol      = "http"
+  config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，up.qiniug.com 不同
 end
